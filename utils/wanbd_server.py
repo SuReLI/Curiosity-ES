f4d252ffb939331417a5caeba1bd29a7a2552cb5
Paul-antoineLeTolguenec <paul-antoine.le_tolguenec@ensta-bretagne.org>
2023-06-14 19:49:30 +0200

    wandb URL

--- utils/wanbd_server.py
+++ utils/wanbd_server.py
@@ -6,12 +6,12 @@ class WandbServer:
 		# wandb ENV variables
 		self.hostname=os.environ['HOSTNAME']
 		if 'armos' in self.hostname:
-			os.environ["WANDB_API_KEY"] = "local-29d7f31a540eb70ca381cf9e7e2ea04895912300"
-			os.environ["WANDB_BASE_URL"] = "http://armos-lx10.tls.fr.eu.airbus.corp:8100"
+			os.environ["WANDB_API_KEY"] = "key"
+			os.environ["WANDB_BASE_URL"] = "http://localhost"
 			os.environ["WANDB_MODE"] = "dryrun"
 			os.environ["WANDB_DIR"] = "../"
 		else : 
-			os.environ["WANDB_API_KEY"] = "524a47ab6550c03c32c0e9c5df8e6a4c404c2bd5"
+			os.environ["WANDB_API_KEY"] = "key"
 			os.environ["WANDB_MODE"] = "dryrun"
 			os.environ["WANDB_DIR"] = "../"
 		# init
